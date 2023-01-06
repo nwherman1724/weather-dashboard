238853e8bdf80ce23ab9ac64dbf37a3bd5f384ab
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-01-06 07:47:46 -0500

    changed units on api response

--- Develop/script.js
+++ Develop/script.js
@@ -14,9 +14,13 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat=42.1167&lon=-86.4542
 
     console.log(city);
 
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=16c3d00c60dcdcff5c5a0a91a5169a6a`)
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=16c3d00c60dcdcff5c5a0a91a5169a6a&units=imperial`)
     .then((response) => response.json())
     .then((data) => console.log(data));
   }
 
   clickSearch.addEventListener('click', search);
+
+  clickSearch.addEventListener('click', () => {
+    
+  });
