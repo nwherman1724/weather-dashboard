6c6892950e7e4e317d980ae5c3f882d409c6e645
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-01-07 21:28:17 -0500

    added 2nd fetch & updated ids

--- Develop/script.js
+++ Develop/script.js
@@ -12,23 +12,36 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat=42.1167&lon=-86.4542
 
     let city = document.querySelector('.city').value;
 
+    localStorage.setItem('searchedCity', city);
+
+    let savedCity = localStorage.getItem('searchedCity');
+  
+    let list = document.querySelector('#searchLog');
+    
+    let p = document.createElement("p")
+    
+    list.append(p);
+      p.textContent = savedCity;
+
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=16c3d00c60dcdcff5c5a0a91a5169a6a&units=imperial`)
     .then((response) => response.json())
     .then((data) => {
       const cityEl = document.getElementById('cityName');
-      cityEl.textContent = `${data.name}` + " " + "(" + dayjs().format('DD/MM/YYYY') + ")";
+      cityEl.textContent = `${data.name}` + " " + "(" + dayjs().format('DD/MM/YYYY') + ")" + " " + `${data.weather.icon}`;
+      const tempEl = document.getElementById('temp');
+      tempEl.textContent = "Temp: " + `${data.main.temp}`
+
+      const windEl = document.getElementById('wind');
+      windEl.textContent = "Wind: " + `${data.wind.speed}`
+
+      const humidEl = document.getElementById('humid');
+      humidEl.textContent = "Humidity: " + `${data.main.humidity}`
+
       console.log(data)});
 
-  localStorage.setItem('searchedCity', city);
-
-  let savedCity = localStorage.getItem('searchedCity');
-
-  let list = document.querySelector('#searchLog');
-  
-  let p = document.createElement("p")
-  
-  list.append(p);
-    p.textContent = savedCity;
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=16c3d00c60dcdcff5c5a0a91a5169a6a&units=imperial`)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
 
   //localStorage.setItem('list',list.value)
     
